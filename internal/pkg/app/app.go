c6a2671629b13b5a71eca29daa7b528900aa9f88
Владислав Бахмацкий <v.bakhmatsky@webpractik.ru>
2023-08-30 01:05:32 +0300

    feat(logger): added slog logger

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -3,20 +3,25 @@ package app
 import (
 	"linklifyBackend/internal/config"
 	"linklifyBackend/internal/linkShortener"
+	"log/slog"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type App struct {
-	ls   *linkShortener.LinkShortener
-	fApp *fiber.App
+	ls     *linkShortener.LinkShortener
+	fApp   *fiber.App
+	logger *slog.Logger
 }
 
 func New() *App {
 	if err := config.InitializeConfig(); err != nil {
 		panic(err)
 	}
-	
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	fApp := fiber.New(fiber.Config{
 		Prefork:       true,
 		CaseSensitive: true,
@@ -28,8 +33,9 @@ func New() *App {
 	ls := linkShortener.New(fApp)
 
 	return &App{
-		fApp: fApp,
-		ls:   ls,
+		fApp:   fApp,
+		ls:     ls,
+		logger: logger,
 	}
 }
 
@@ -37,6 +43,8 @@ func (a *App) Run() error {
 	if err := a.ls.RegisterLinkShortenerRoutes(); err != nil {
 		return err
 	}
+
+	a.logger.Info("Logger initialized")
 
 	a.fApp.Listen(":" + config.AppConfig.Port)
 
